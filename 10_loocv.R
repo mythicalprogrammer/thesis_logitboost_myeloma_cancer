39511ae94f6aaa21c94370696e28d30343ea3490
Anthony Doan <mythicalprogrammer@gmail.com>
2019-03-10 18:48:18 -0700

    Added cross validation code and result.

---
+++ 10_loocv.R
@@ -0,0 +1,34 @@
+myeloma_data <- read.table("start_data/GDS531_after_anova.csv",
+                            sep = ",",
+                            header = TRUE)
+
+xlearn <- myeloma_data
+xlearn$state <- NULL
+ylearn <- myeloma_data$state
+
+
+rand_seed <- 1030
+set.seed(rand_seed)
+
+pred <- c()
+j <- 1
+for (i in 1:nrow(xlearn)) {
+  leftout_row <- xlearn[i,]
+  #leftout_col <- ylearn[i,]
+  kth_fold_train <- xlearn[-i,]
+  kth_fold_response <- ylearn[-i]
+
+  kth_fit <-
+    LogitBoost(kth_fold_train,
+               kth_fold_response,
+               nIter = ncol(kth_fold_train))
+
+  pred[j] <- predict(kth_fit, leftout_row, type = "class")
+  j <- j + 1
+}
+
+# back up the result since this took awhile to run
+file_path  <- 'result_logitboost.csv'
+write.csv(pred,
+          file = file_path,
+          row.names = FALSE)
